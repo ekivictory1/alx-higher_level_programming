2f6e33747d7093f80e324172d94b498795744559
Eki Victory <106748977+ekivictory1@users.noreply.github.com>
2022-10-25 22:12:53 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
-filename = "add_item.json"
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
