2babcbc8638c4ffe2263cf307f6e7bbde63bb6b5
Eki Victory <106748977+ekivictory1@users.noreply.github.com>
2022-10-25 21:53:17 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,8 +12,5 @@ filename = "add_item.json"
 with open(filename, 'a+', encoding="utf-8") as f:
     my_list = []
     my_list.extend(args[1:])
-    my_list.extend(sys.argv[1:])
-    my_list.append(args[1:])
-    my_list.append(sys.argv[1:])
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
