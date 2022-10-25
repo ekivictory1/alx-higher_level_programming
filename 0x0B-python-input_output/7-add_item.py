a50ba38cf8a85050f3e08d6cda5c89fbcd93c033
Eki Victory <106748977+ekivictory1@users.noreply.github.com>
2022-10-25 21:50:59 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+""" Add all arguments to a Python list and save them to a file """
 import sys
+import json
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+args = sys.argv
+filename = "add_item.json"
+with open(filename, 'a+', encoding="utf-8") as f:
+    my_list = []
+    my_list.extend(args[1:])
+    my_list.extend(sys.argv[1:])
+    my_list.append(args[1:])
+    my_list.append(sys.argv[1:])
+    save_to_json_file(my_list, filename)
+    load_from_json_file(filename)
